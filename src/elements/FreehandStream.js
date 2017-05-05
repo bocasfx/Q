502c517b0b833d878be7b66d62585be34083ec9c
Rodolfo Palacios <rpalacios@igloosoftware.com>
2017-05-05 16:20:19 -0400

    Stream subclasses.

---
+++ src/elements/FreehandStream.js
@@ -0,0 +1,139 @@
+import config from '../config/config';
+import Particle from './Particle';
+import { getPosition } from '../utils/utils';
+import Stream from './Stream';
+
+class FreehandStream extends Stream {
+  constructor(position) {
+    super(position);
+    this.queue = [];
+    this.easing = '';
+    this.headPosition = position;
+    this.pathIndex = 0;
+    this.path = [];
+    this.class ='freehand';
+
+    for (let i = 0; i < this.count; i++) {
+      this.particles.push(new Particle(position));
+    }
+
+    for (let i = 0; i < config.stream.size; i++) {
+      this.queue.push(position);
+    }
+
+    this.markerImage = new Image();
+    this.markerImage.src = './icons/elements/stream-marker.png';
+  }
+
+  advancePathIndex() {
+
+    this.pathIndex++;
+    if (this.pathIndex > (this.path.length - 1)) {
+      this.pathIndex = 0;
+    }
+  }
+
+  calculateEasing(position, easing) {
+    let dx = position[0] - easing[0];
+    easing = [easing[0] + (dx * config.stream.easingFactor), easing[1]];
+
+    let dy = position[1] - easing[1];
+    easing = [easing[0], easing[1] + (dy * config.stream.easingFactor)];
+
+    return easing;
+  }
+
+  onMouseDown(event) {
+    event.preventDefault();
+    this.headPosition = getPosition(event);
+    this.path = [];
+    this.pathIndex = 0;
+    this.path.push(this.headPosition);
+    this.mouseDown = true;
+    this.showMarker = true;
+    this.markerPosition = getPosition(event);
+  }
+
+  onMouseUp(event) {
+    event.preventDefault();
+    this.mouseDown = false;
+    this.showMarker = false;
+  }
+
+  onMouseMove(event) {
+    event.preventDefault();
+    this.headPosition = getPosition(event);
+  }
+
+  flow() {
+    if (this.disabled) {
+      return;
+    }
+    if (this.headPosition !== '') {
+      if (!this.mouseDown) {
+        if (this.path.length) {
+          this.headPosition = this.path[this.pathIndex];
+          this.advancePathIndex();
+        }
+      } else {
+        this.path.push(this.headPosition);
+        if (this.queue.length < config.stream.size) {
+          this.queue.push(this.headPosition);
+        }
+      }
+      if (this.easing === '') {
+        this.easing = this.headPosition;
+      }
+      this.easing = this.calculateEasing(this.headPosition, this.easing, config.stream.easingFactor);
+      this.queue[0] = this.easing;
+      this.queue.push(this.queue.shift());
+
+      if (this.particles.length) {
+
+        let pointInRope = 0;
+        let particleIdx = 0;
+        let step = this.particles.length > 1 ? Math.floor(this.queue.length / (this.particles.length - 1)) : this.queue.length * this.speed;
+
+        while (pointInRope <= this.queue.length - step) {
+          this.particles[particleIdx].position = this.queue[pointInRope];
+          if ((pointInRope + step * 2) > this.queue.length) {
+            pointInRope = this.queue.length - 1;
+          } else {
+            pointInRope += step;
+          }
+          particleIdx++;
+        }
+
+        this.particles[this.particles.length - 1].position = this.queue[this.queue.length - 1];
+      }
+    }
+  }
+
+  render(canvasContext) {
+
+    canvasContext.beginPath();
+    canvasContext.strokeStyle = this.selected ? config.selectedStream.strokeStyle : config.stream.strokeStyle;
+    canvasContext.strokeStyle = this.disabled ? config.stream.strokeStyle : canvasContext.strokeStyle;
+    canvasContext.lineWidth = config.stream.lineWidth;
+    canvasContext.setLineDash(config.stream.lineDash);
+
+    for (let i=1; i<this.queue.length; i++) {
+      
+      canvasContext.moveTo(this.queue[i-1][0], this.queue[i-1][1]);
+      canvasContext.lineTo(this.queue[i][0], this.queue[i][1]);
+    }
+
+    canvasContext.stroke();
+
+    this.particles.forEach((particle) => {
+      particle.disabled = this.disabled;
+      particle.render(canvasContext);
+    });
+
+    if (this.showMarker) {
+      canvasContext.drawImage(this.markerImage, this.markerPosition[0] - 7.5, this.markerPosition[1] - 7.5);
+    }
+  }
+}
+
+module.exports = FreehandStream;
--- src/elements/Stream.js
+++ src/elements/Stream.js
@@ -1,159 +1,18 @@
-import config from '../config/config';
-import Particle from './Particle';
 import uuidv1 from 'uuid/v1';
 import names from '../config/names';
-import { getPosition } from '../utils/utils';
+import config from '../config/config';
 
 class Stream {
-  constructor(position) {
-
+  constructor() {
     this.id = uuidv1();
-
-    let x = position[0];
-    let y = position[1];
-
-    let particles = [];
-    this.count = config.particle.count;
-    for (let i = 0; i < this.count; i++) {
-      particles.push(new Particle([x, y]));
-    }
-
-    let queue = [];
-    for (let i = 0; i < config.stream.size; i++) {
-      queue.push([x, y]);
-    }
-
-    this.queue = queue;
-    this.easing = '';
-    this.mouseState = 'up';
-    this.headPosition = [x, y];
-    this.pathIndex = 0;
-    this.path = [];
-    this.particles = particles;
     this.name = names.generate();
-    this.selected = false;
-    this.type = 'stream';
-    this.class ='freehand';
     this.disabled = false;
+    this.type = 'stream';
+    this.mouseDown = false;
+    this.particles = [];
     this.speed = 1.0;
-
-    this.calculateEasing = this.calculateEasing.bind(this);
-    this.onMouseDown = this.onMouseDown.bind(this);
-    this.onMouseMove = this.onMouseMove.bind(this);
-    this.onMouseUp = this.onMouseUp.bind(this);
-
-    this.markerImage = new Image();
-    this.markerImage.src = './icons/elements/stream-marker.png';
-  }
-
-  advancePathIndex() {
-
-    this.pathIndex++;
-    if (this.pathIndex > (this.path.length - 1)) {
-      this.pathIndex = 0;
-    }
-  }
-
-  calculateEasing(position, easing) {
-    let dx = position[0] - easing[0];
-    easing = [easing[0] + (dx * config.stream.easingFactor), easing[1]];
-
-    let dy = position[1] - easing[1];
-    easing = [easing[0], easing[1] + (dy * config.stream.easingFactor)];
-
-    return easing;
-  }
-
-  onMouseDown(event) {
-    event.preventDefault();
-    this.headPosition = getPosition(event);
-    this.path = [];
-    this.pathIndex = 0;
-    this.path.push(this.headPosition);
-    this.mouseState = 'down';
-    this.showMarker = true;
-    this.markerPosition = getPosition(event);
-  }
-
-  onMouseUp(event) {
-    event.preventDefault();
-    this.mouseState = 'up';
-    this.showMarker = false;
-  }
-
-  onMouseMove(event) {
-    event.preventDefault();
-    this.headPosition = getPosition(event);
-  }
-
-  flow() {
-    if (this.disabled) {
-      return;
-    }
-    if (this.headPosition !== '') {
-      if (this.mouseState === 'up') {
-        if (this.path.length) {
-          this.headPosition = this.path[this.pathIndex];
-          this.advancePathIndex();
-        }
-      } else {
-        this.path.push(this.headPosition);
-        if (this.queue.length < config.stream.size) {
-          this.queue.push(this.headPosition);
-        }
-      }
-      if (this.easing === '') {
-        this.easing = this.headPosition;
-      }
-      this.easing = this.calculateEasing(this.headPosition, this.easing, config.stream.easingFactor);
-      this.queue[0] = this.easing;
-      this.queue.push(this.queue.shift());
-
-      if (this.particles.length) {
-
-        let pointInRope = 0;
-        let particleIdx = 0;
-        let step = this.particles.length > 1 ? Math.floor(this.queue.length / (this.particles.length - 1)) : this.queue.length * this.speed;
-
-        while (pointInRope <= this.queue.length - step) {
-          this.particles[particleIdx].position = this.queue[pointInRope];
-          if ((pointInRope + step * 2) > this.queue.length) {
-            pointInRope = this.queue.length - 1;
-          } else {
-            pointInRope += step;
-          }
-          particleIdx++;
-        }
-
-        this.particles[this.particles.length - 1].position = this.queue[this.queue.length - 1];
-      }
-    }
-  }
-
-  render(canvasContext) {
-
-    canvasContext.beginPath();
-    canvasContext.strokeStyle = this.selected ? config.selectedStream.strokeStyle : config.stream.strokeStyle;
-    canvasContext.strokeStyle = this.disabled ? config.stream.strokeStyle : canvasContext.strokeStyle;
-    canvasContext.lineWidth = config.stream.lineWidth;
-    canvasContext.setLineDash(config.stream.lineDash);
-
-    for (let i=1; i<this.queue.length; i++) {
-      
-      canvasContext.moveTo(this.queue[i-1][0], this.queue[i-1][1]);
-      canvasContext.lineTo(this.queue[i][0], this.queue[i][1]);
-    }
-
-    canvasContext.stroke();
-
-    this.particles.forEach((particle) => {
-      particle.disabled = this.disabled;
-      particle.render(canvasContext);
-    });
-
-    if (this.showMarker) {
-      canvasContext.drawImage(this.markerImage, this.markerPosition[0] - 7.5, this.markerPosition[1] - 7.5);
-    }
+    this.selected = false;
+    this.count = config.particle.count;
   }
 }
 
