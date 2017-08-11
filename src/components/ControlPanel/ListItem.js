4d8f4b4e3bf81ad5febe979c36f05d9f0349ac93
Rodolfo Palacios <rpalacios@igloosoftware.com>
2017-08-11 12:40:13 -0400

    Fixed listItem icon hover.

--- src/components/ControlPanel/ListItem.js
+++ src/components/ControlPanel/ListItem.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import './ListItem.css';
 import ActivityIndicator from '../UI/ActivityIndicator';
+import { getNodeColor } from '../../utils/utils';
 
 class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      off: {},
+      copy: {},
+      trash: {}
+    };
+
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+
+  onMouseEnter(event) {
+    let type = event.target.getAttribute('data-type');
+    let newState = {};
+    newState[type] = {
+      color: getNodeColor(this.props.item.type)
+    };
+    this.setState(newState);
+  }
+
+  onMouseLeave() {
+    this.setState({
+      off: {
+        color: 'darkgray'
+      },
+      copy: {
+        color: 'darkgray'
+      },
+      trash: {
+        color: 'darkgray'
+      }
+    });
+  }
+
   render() {
 
     let itemClass = 'list-item-container';
@@ -16,13 +52,13 @@ class ListItem extends React.Component {
         </div>
         <div>
           <span className="list-item-icon" onClick={this.props.onToggle}>
-            <i className="fa fa-power-off"></i>
+            <i data-type="off" className="fa fa-power-off" style={this.state.off} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}></i>
           </span>
           <span className="list-item-icon" onClick={this.props.onClone}>
-            <i className="fa fa-copy"></i>
+            <i data-type="copy" className="fa fa-copy" style={this.state.copy} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}></i>
           </span>
           <span className="list-item-icon" onClick={this.props.onDelete}>
-            <i className="fa fa-trash"></i>
+            <i data-type="trash" className="fa fa-trash" style={this.state.trash} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}></i>
           </span>
         </div>
       </div>
