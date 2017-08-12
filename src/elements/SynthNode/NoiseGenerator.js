1540be5a470ea5e17bdb3f5eb70b456b03822b1f
Rodolfo Palacios <rpalacios@igloosoftware.com>
2017-08-12 19:30:22 -0400

    Generate pink noise with createScriptProcessor.

--- src/elements/SynthNode/NoiseGenerator.js
+++ src/elements/SynthNode/NoiseGenerator.js
@@ -4,6 +4,10 @@ class NoiseGenerator {
 
   constructor() {
 
+    this.generatePinkNoise();
+  }
+
+  generateWhiteNoise() {
     let bufferSize = 2 * qAudioContext.ctx.sampleRate;
     let noiseBuffer = qAudioContext.ctx.createBuffer(1, bufferSize, qAudioContext.ctx.sampleRate);
     let output = noiseBuffer.getChannelData(0);
@@ -16,6 +20,32 @@ class NoiseGenerator {
     this.generator.buffer = noiseBuffer;
     this.generator.loop = true;
     this.generator.start(0);
+  }
+
+  generatePinkNoise() {
+    let bufferSize = 4096;
+    this.generator = (() => {
+        let b0, b1, b2, b3, b4, b5, b6;
+        b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+
+        let node = qAudioContext.ctx.createScriptProcessor(bufferSize, 1, 1);
+        node.onaudioprocess = (event) => {
+            let output = event.outputBuffer.getChannelData(0);
+            for (var i = 0; i < bufferSize; i++) {
+              let white = Math.random() * 2 - 1;
+              b0 = 0.99886 * b0 + white * 0.0555179;
+              b1 = 0.99332 * b1 + white * 0.0750759;
+              b2 = 0.96900 * b2 + white * 0.1538520;
+              b3 = 0.86650 * b3 + white * 0.3104856;
+              b4 = 0.55000 * b4 + white * 0.5329522;
+              b5 = -0.7616 * b5 - white * 0.0168980;
+              output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+              output[i] *= 0.11; // (roughly) compensate for gain
+              b6 = white * 0.115926;
+            }
+        };
+        return node;
+    })();
   }
 
   connect(node) {
