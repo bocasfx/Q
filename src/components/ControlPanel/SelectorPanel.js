a6173fa77e849775dc8ea8329680a1bd5a20baa7
Rodolfo Palacios <rpalacios@igloosoftware.com>
2017-08-14 11:09:59 -0400

    Filter panel.

--- src/components/ControlPanel/SelectorPanel.js
+++ src/components/ControlPanel/SelectorPanel.js
@@ -7,6 +7,7 @@ import { bindActionCreators } from 'redux';
 import { deleteStream, selectStream, setStreamDisabledStatus } from '../../actions/Streams';
 import { deleteNode, selectNode, setNodeDisabledStatus, deselectNodes } from '../../actions/Nodes';
 import FXPanel from './FXPanel';
+import FilterPanel from './FilterPanel';
 import './SelectorPanel.css';
 
 class SelectorPanel extends React.Component {
@@ -95,6 +96,10 @@ class SelectorPanel extends React.Component {
     return <FXPanel />;
   }
 
+  renderFilter() {
+    return <FilterPanel />;
+  }
+
   render() {
     return (
       <div className="selector-panel-container">
@@ -107,6 +112,9 @@ class SelectorPanel extends React.Component {
           </Panel>
           <Panel label="FX">
             {this.renderFx()}
+          </Panel>
+          <Panel label="Filter">
+            {this.renderFilter()}
           </Panel>
         </Tabs>
       </div>
