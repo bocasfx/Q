fd887e90323a9e7ba52e85435d4ea90c179f28bf
Rodolfo Palacios <rpalacios@igloosoftware.com>
2017-08-24 11:31:39 -0400

    Fixed scrolling issues. Upgraded packages.

--- src/components/UI/Tabs/Panel.js
+++ src/components/UI/Tabs/Panel.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './Panel.css';
 
 class Panel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onKeyPress = this.onKeyPress.bind(this);
+  }
+
+  onKeyPress(event) {
+    if(event.keyCode === 0) {
+      event.preventDefault();
+    }
+  }
+
   render() {
     return (
-      <div className="panel-container">
+      <div className="panel-container" onKeyPress={this.onKeyPress} tabIndex="1">
         <div className="panel-list">
           {this.props.children}
         </div>
