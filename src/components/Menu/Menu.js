d04397f55affdb99a1610c072b091ce61b270e18
Rodolfo Palacios <rpalacios@igloosoftware.com>
2017-09-07 13:33:40 -0400

    Handle mouse leave in canvas. Added menu button titles.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -18,56 +18,67 @@ class Menu extends React.Component {
           icon="./icons/menu/grab.svg"
           onClick={this.onClick.bind(this, 'grab')}
           active={this.props.devices.grab}
+          title="Grab nodes"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/synth.svg"
           onClick={this.onClick.bind(this, 'synthNodes')}
           active={this.props.devices.synthNodes}
+          title="Synth node"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/midi.svg"
           onClick={this.onClick.bind(this, 'midiNodes')}
           active={this.props.devices.midiNodes}
+          title="MIDI node"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/audio.svg"
           onClick={this.onClick.bind(this, 'audioNodes')}
           active={this.props.devices.audioNodes}
+          title="Audio node"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/linear-stream.svg"
           onClick={this.onClick.bind(this, 'linearStreams')}
           active={this.props.devices.linearStreams}
+          title="Linear stream"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/circular-stream.svg"
           onClick={this.onClick.bind(this, 'circularStreams')}
           active={this.props.devices.circularStreams}
+          title="Circular stream"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/stream.svg"
           onClick={this.onClick.bind(this, 'streams')}
           active={this.props.devices.streams}
+          title="Freehand stream"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/clone.svg"
           onClick={this.onClick.bind(this, 'clone')}
           active={this.props.devices.clone}
+          title="Clone node"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/link.svg"
           onClick={this.onClick.bind(this, 'link')}
           active={this.props.devices.link}
+          title="Link nodes"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/unlink.svg"
           onClick={this.onClick.bind(this, 'unlink')}
           active={this.props.devices.unlink}
+          title="Unlink nodes"
           separator={true}/>
         <MenuButton
           icon="./icons/menu/mixer.svg"
           onClick={this.onClick.bind(this, 'mixer')}
-          active={this.props.devices.mixer}/>
+          active={this.props.devices.mixer}
+          title="Mixer"/>
       </div>
     );
   }
--- src/components/Menu/MenuButton.js
+++ src/components/Menu/MenuButton.js
@@ -8,7 +8,7 @@ class MenuButton extends React.Component {
     menuClass += this.props.active ? ' menu-button-active' : '';
     
     return (
-      <div className="menu-button-container">
+      <div className="menu-button-container" title={this.props.title}>
         <div className={menuClass} onClick={this.props.onClick}>
           <img className="menu-button-icon" src={this.props.icon} alt="icon" draggable={false}/>
         </div>
